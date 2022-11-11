ca825041897eba9e4cf605fa174639c91a9f431d
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-11 17:17:44 +0100

    render people

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
+import people from './mock-data/person';
 import widgets from './mock-data/widget';
 
 function App() {
   return (
     <div className="App">
+      <h1>Widgets:</h1>
       {widgets.map(widget => {
         return (
           <div key={widget.id}>
             <h2>{widget.title}</h2>
-            <p>{widget.description}</p>
+            {/* <p>{widget.description}</p> */}
+          </div>
+        );
+      })}
+      <h1>People:</h1>
+      {people.map(person => {
+        return (
+          <div key={person.eyeColor}>
+            <h2>{person.firstName} - {person.lastName}</h2>
           </div>
         );
       })}
