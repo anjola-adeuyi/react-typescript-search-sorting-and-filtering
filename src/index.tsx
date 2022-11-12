18c61de2bc637c76163e96b355d8d3d8698405d9
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-12 19:54:02 +0100

    update scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles/styles.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
